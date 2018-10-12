9378daee881977ba0473754dd547d237f1c3e8af
Benoit Dubreuil <lol12lol12lol12lol12@gmail.com>
2018-10-12 12:02:48 -0400

    Adjusted the ball's friction. Added the BallControler script.

---
+++ Assets/Source/BallController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    public float torque;
+    
+    private Rigidbody rigidbody;
+
+    public BallController()
+    {
+        torque = 15f;
+    }
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float xAxis = -Input.GetAxis("Horizontal");
+        float zAxis = -Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(xAxis, 0, zAxis).normalized;
+        direction.Normalize();
+
+        Vector3 torqueAxis = Vector3.Cross(direction, Vector3.up);
+        torqueAxis.Normalize();
+
+        rigidbody.AddTorque(torqueAxis * torque, ForceMode.Acceleration);
+
+        Debug.DrawLine(transform.position, transform.position + torqueAxis, Color.magenta, Time.fixedDeltaTime);
+    }
+}
